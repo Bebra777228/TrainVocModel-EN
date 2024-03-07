7f3c7dfd5e060776ffdfa07d8cb44739b28e66a4
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-03-07 15:58:51 +0500

    Update download_files.py

--- download_files.py
+++ download_files.py
@@ -1,15 +1,18 @@
-import subprocess, os
+import os
+import subprocess
+
 assets_folder = "./assets/"
-if not os.path.exists(assets_folder):
-    os.makedirs(assets_folder)
-files = {
-    "rmvpe/rmvpe.pt":"https://huggingface.co/Rejekts/project/resolve/main/rmvpe.pt",
-    "hubert/hubert_base.pt":"https://huggingface.co/Rejekts/project/resolve/main/hubert_base.pt"
+os.makedirs(assets_folder, exist_ok=True)
+
+file_links = {
+    "rmvpe/rmvpe.pt": "https://huggingface.co/Rejekts/project/resolve/main/rmvpe.pt",
+    "hubert/hubert_base.pt": "https://huggingface.co/Rejekts/project/resolve/main/hubert_base.pt"
 }
-for file, link in files.items():
+
+for file, link in file_links.items():
     file_path = os.path.join(assets_folder, file)
     if not os.path.exists(file_path):
         try:
-            subprocess.run(['wget', link, '-O', file_path], check=True)
+            subprocess.run(['wget', '-O', file_path, link], check=True)
         except subprocess.CalledProcessError as e:
             print(f"Error downloading {file}: {e}")
