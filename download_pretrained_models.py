ba439235208c80d6d470fd340942a1472e3e26e2
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-03-07 16:00:45 +0500

    Update download_pretrained_models.py

--- download_pretrained_models.py
+++ download_pretrained_models.py
@@ -1,21 +1,26 @@
-import subprocess, os
+import os
+import subprocess
+
 pretrained_folder = "/content/pretrained_models"
-if not os.path.exists(pretrained_folder):
-    os.makedirs(pretrained_folder)
+os.makedirs(pretrained_folder, exist_ok=True)
+
 files = {
-    "f0D40k.pth":"https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/pretrained_v2/f0D40k.pth",
-    "f0G40k.pth":"https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/pretrained_v2/f0G40k.pth",
-    "f0Ov2Super40kD.pth":"https://huggingface.co/ORVC/Ov2Super/resolve/main/f0Ov2Super40kD.pth",
-    "f0Ov2Super40kG.pth":"https://huggingface.co/ORVC/Ov2Super/resolve/main/f0Ov2Super40kG.pth",
-    "f0SnowieRuPre40kD.pth":"https://huggingface.co/MUSTAR/SnowyRuPretrain_EnP_40k/resolve/main/D_Snowie_RuPretrain_EnP.pth",
-    "f0SnowieRuPre40kG.pth":"https://huggingface.co/MUSTAR/SnowyRuPretrain_EnP_40k/resolve/main/G_Snowie_RuPretrain_EnP.pth",
-    "f0_Rin_E3_40kD.pth":"https://huggingface.co/ORVC/RIN_E3/resolve/main/RIN_E3_D.pth",
-    "f0_Rin_E3_40kG.pth":"https://huggingface.co/ORVC/RIN_E3/resolve/main/RIN_E3_G.pth"
+    "f0D40k.pth": "https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/pretrained_v2/f0D40k.pth",
+    "f0G40k.pth": "https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/pretrained_v2/f0G40k.pth",
+    "f0Ov2Super40kD.pth": "https://huggingface.co/ORVC/Ov2Super/resolve/main/f0Ov2Super40kD.pth",
+    "f0Ov2Super40kG.pth": "https://huggingface.co/ORVC/Ov2Super/resolve/main/f0Ov2Super40kG.pth",
+    "f0SnowieRuPre40kD.pth": "https://huggingface.co/MUSTAR/SnowyRuPretrain_EnP_40k/resolve/main/D_Snowie_RuPretrain_EnP.pth",
+    "f0SnowieRuPre40kG.pth": "https://huggingface.co/MUSTAR/SnowyRuPretrain_EnP_40k/resolve/main/G_Snowie_RuPretrain_EnP.pth",
+    "f0_Rin_E3_40kD.pth": "https://huggingface.co/ORVC/RIN_E3/resolve/main/RIN_E3_D.pth",
+    "f0_Rin_E3_40kG.pth": "https://huggingface.co/ORVC/RIN_E3/resolve/main/RIN_E3_G.pth"
 }
+
+aria2c_args = ['aria2c', '--console-log-level=error', '-c', '-x', '16', '-s', '16', '-k', '1M']
+
 for file, link in files.items():
     file_path = os.path.join(pretrained_folder, file)
     if not os.path.exists(file_path):
         try:
-            subprocess.run(['aria2c', '--console-log-level=error', '-c', '-x', '16', '-s', '16', '-k', '1M', link, '-d', pretrained_folder, '-o', file], check=True)
+            subprocess.run(aria2c_args + [link, '-d', pretrained_folder, '-o', file], check=True)
         except subprocess.CalledProcessError as e:
             print(f"Error downloading {file}: {e}")
