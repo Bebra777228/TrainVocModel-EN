055d784224859ed334c41fd47510ec3db29f6efb
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-03-09 02:20:19 +0500

    Update install.py

--- install.py
+++ install.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 from tqdm import tqdm
+from tqdm.contrib.logging import logging_redirect_tqdm
 
 # Установка зависимостей
 print('Установка зависимостей...')
@@ -41,29 +42,12 @@ for file, link in files.items():
     file_path = os.path.join(pretrained_folder, file)
     if not os.path.exists(file_path):
         try:
-            # Запускаем команду загрузки файла с выводом в консоль
-            process = subprocess.Popen(['aria2c', '--console-log-level=info', '-c', '-x', '16', '-s', '16', '-k', '1M', link, '-d', pretrained_folder, '-o', file], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
-            # Читаем вывод команды и обновляем полосу прогресса
-            progress = None
-            for line in iter(process.stdout.readline, b''):
-                line = line.decode().strip()
-                if progress is None and 'Progress' in line:
-                    progress = line.split('=')[1].strip()
-                elif progress is not None:
-                    new_progress = line.split('=')[1].strip()
-                    if new_progress != progress:
-                        progress = new_progress
-                        progress_bar.set_description(f"Downloading {file}: {progress}")
-                        progress_bar.update(1)
-
-            # Дожидаемся завершения команды
-            process.wait()
-
+            # Перенаправление вывода в консоль на полосу прогресса
+            with logging_redirect_tqdm():
+                subprocess.run(['aria2c', '--console-log-level=info', '-c', '-x', '16', '-s', '16', '-k', '1M', link, '-d', pretrained_folder, '-o', file], check=True)
         except subprocess.CalledProcessError as e:
             print(f"Error downloading {file}: {e}")
-    else:
-        progress_bar.update(1)
+    progress_bar.update(1)
 progress_bar.close()
 
 # Загрузка дополнительных файлов
@@ -80,29 +64,12 @@ for file, link in file_links.items():
     file_path = os.path.join(assets_folder, file)
     if not os.path.exists(file_path):
         try:
-            # Запускаем команду загрузки файла с выводом в консоль
-            process = subprocess.Popen(['wget', '-O', file_path, link], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
-            # Читаем вывод команды и обновляем полосу прогресса
-            progress = None
-            for line in iter(process.stdout.readline, b''):
-                line = line.decode().strip()
-                if progress is None and '[' in line and ']' in line:
-                    progress = line.split('[')[1].split(']')[0].strip()
-                elif progress is not None:
-                    new_progress = line.split('[')[1].split(']')[0].strip()
-                    if new_progress != progress:
-                        progress = new_progress
-                        progress_bar.set_description(f"Downloading {file}: {progress}")
-                        progress_bar.update(1)
-
-            # Дожидаемся завершения команды
-            process.wait()
-
+            # Перенаправление вывода в консоль на полосу прогресса
+            with logging_redirect_tqdm():
+                subprocess.run(['wget', '-O', file_path, link], check=True)
         except subprocess.CalledProcessError as e:
             print(f"Error downloading {file}: {e}")
-    else:
-        progress_bar.update(1)
+    progress_bar.update(1)
 progress_bar.close()
 
 print("\u2714 Готово")
