d85c3f2f78343a972e32d212af18759e35b6e9f4
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-03-10 23:54:12 +0500

    Update download_pretrained_models.py

--- download_pretrained_models.py
+++ download_pretrained_models.py
@@ -3,14 +3,18 @@ pretrained_folder = "/content/pretrained_models"
 if not os.path.exists(pretrained_folder):
     os.makedirs(pretrained_folder)
 files = {
-    "f0D40k.pth":"https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/pretrained_v2/f0D40k.pth",
-    "f0G40k.pth":"https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/pretrained_v2/f0G40k.pth",
-    "f0Ov2Super40kD.pth":"https://huggingface.co/ORVC/Ov2Super/resolve/main/f0Ov2Super40kD.pth",
-    "f0Ov2Super40kG.pth":"https://huggingface.co/ORVC/Ov2Super/resolve/main/f0Ov2Super40kG.pth",
-    "f0SnowieRuPre40kD.pth":"https://huggingface.co/MUSTAR/SnowyRuPretrain_EnP_40k/resolve/main/D_Snowie_RuPretrain_EnP.pth",
-    "f0SnowieRuPre40kG.pth":"https://huggingface.co/MUSTAR/SnowyRuPretrain_EnP_40k/resolve/main/G_Snowie_RuPretrain_EnP.pth",
-    "f0_Rin_E3_40kD.pth":"https://huggingface.co/ORVC/RIN_E3/resolve/main/RIN_E3_D.pth",
-    "f0_Rin_E3_40kG.pth":"https://huggingface.co/ORVC/RIN_E3/resolve/main/RIN_E3_G.pth"
+    "default_D.pth":"https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/pretrained_v2/f0D40k.pth",
+    "default_G.pth":"https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/pretrained_v2/f0G40k.pth",
+    "Ov2Super_D.pth":"https://huggingface.co/ORVC/Ov2Super/resolve/main/f0Ov2Super40kD.pth",
+    "Ov2Super_G.pth":"https://huggingface.co/ORVC/Ov2Super/resolve/main/f0Ov2Super40kG.pth",
+    "SnowieV2_D.pth":"https://huggingface.co/MUSTAR/SnowyRuPretrain_EnP_40k/resolve/main/D_Snowie_RuPretrain_EnP.pth",
+    "SnowieV2_G.pth":"https://huggingface.co/MUSTAR/SnowyRuPretrain_EnP_40k/resolve/main/G_Snowie_RuPretrain_EnP.pth",
+    "Rin_E3_D.pth":"https://huggingface.co/ORVC/RIN_E3/resolve/main/RIN_E3_D.pth",
+    "Rin_E3_G.pth":"https://huggingface.co/ORVC/RIN_E3/resolve/main/RIN_E3_G.pth",
+    "SnowieV3_D.pth":"https://huggingface.co/MUSTAR/SnowieV3-40k_pretrain/resolve/main/D_SnowieV3_40k.pth",
+    "SnowieV3_G.pth":"https://huggingface.co/MUSTAR/SnowieV3-40k_pretrain/resolve/main/G_SnowieV3_40k.pth",
+    "SnowieV3_x_Rin_E3_D.pth":"https://huggingface.co/MUSTAR/SnowieV3-X-RINE3-40K/resolve/main/D_Snowie-X-Rin_40k.pth",
+    "SnowieV3_x_Rin_E3_G.pth":"https://huggingface.co/MUSTAR/SnowieV3-X-RINE3-40K/resolve/main/G_Snowie-X-Rin_40k.pth"
 }
 for file, link in files.items():
     file_path = os.path.join(pretrained_folder, file)
