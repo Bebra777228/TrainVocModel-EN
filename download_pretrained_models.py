21372fd9bb8471b71a62acf24a31c3cfeed02d25
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-03-11 17:26:59 +0500

    Update download_pretrained_models.py

--- download_pretrained_models.py
+++ download_pretrained_models.py
@@ -3,14 +3,14 @@ pretrained_folder = "./assets/pretrained_v2"
 if not os.path.exists(pretrained_folder):
     os.makedirs(pretrained_folder)
 files = {
-    "default_D.pth":"https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/pretrained_v2/f0D40k.pth",
-    "default_G.pth":"https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/pretrained_v2/f0G40k.pth",
-    "Ov2Super_D.pth":"https://huggingface.co/ORVC/Ov2Super/resolve/main/f0Ov2Super40kD.pth",
-    "Ov2Super_G.pth":"https://huggingface.co/ORVC/Ov2Super/resolve/main/f0Ov2Super40kG.pth",
-    "SnowieV2_D.pth":"https://huggingface.co/MUSTAR/SnowyRuPretrain_EnP_40k/resolve/main/D_Snowie_RuPretrain_EnP.pth",
-    "SnowieV2_G.pth":"https://huggingface.co/MUSTAR/SnowyRuPretrain_EnP_40k/resolve/main/G_Snowie_RuPretrain_EnP.pth",
-    "Rin_E3_D.pth":"https://huggingface.co/ORVC/RIN_E3/resolve/main/RIN_E3_D.pth",
-    "Rin_E3_G.pth":"https://huggingface.co/ORVC/RIN_E3/resolve/main/RIN_E3_G.pth"
+    "default_D_40k.pth":"https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/pretrained_v2/f0D40k.pth",
+    "default_G_40k.pth":"https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/pretrained_v2/f0G40k.pth",
+    "Ov2Super_D_40k.pth":"https://huggingface.co/ORVC/Ov2Super/resolve/main/f0Ov2Super40kD.pth",
+    "Ov2Super_G_40k.pth":"https://huggingface.co/ORVC/Ov2Super/resolve/main/f0Ov2Super40kG.pth",
+    "SnowieV2_D_40k.pth":"https://huggingface.co/MUSTAR/SnowyRuPretrain_EnP_40k/resolve/main/D_Snowie_RuPretrain_EnP.pth",
+    "SnowieV2_G_40k.pth":"https://huggingface.co/MUSTAR/SnowyRuPretrain_EnP_40k/resolve/main/G_Snowie_RuPretrain_EnP.pth",
+    "Rin_E3_D_40k.pth":"https://huggingface.co/ORVC/RIN_E3/resolve/main/RIN_E3_D.pth",
+    "Rin_E3_G_40k.pth":"https://huggingface.co/ORVC/RIN_E3/resolve/main/RIN_E3_G.pth"
 }
 for file, link in files.items():
     file_path = os.path.join(pretrained_folder, file)
