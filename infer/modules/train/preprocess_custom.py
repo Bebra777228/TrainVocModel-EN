9dcc68c1f2aaff013a9bcc917e27ad62e96024c5
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-11-28 02:35:07 +0500

    Update preprocess_custom.py

--- infer/modules/train/preprocess_custom.py
+++ infer/modules/train/preprocess_custom.py
@@ -36,11 +36,11 @@ class PreProcess:
     def __init__(self, sr, exp_dir, per=4.0):
         self.slicer = Slicer(
             sr=sr,
-            threshold=-40,
+            threshold=-42,
             min_length=1500,
             min_interval=100,
-            hop_size=5,
-            max_sil_kept=1000,
+            hop_size=15,
+            max_sil_kept=500,
         )
         self.sr = sr
         self.bh, self.ah = signal.butter(N=5, Wn=48, btype="high", fs=self.sr)
