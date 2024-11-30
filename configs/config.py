ba43c2c8384517ec7b878612d7834b7e0456d8dd
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-11-30 15:58:16 +0500

    .

--- configs/config.py
+++ configs/config.py
@@ -6,16 +6,6 @@ import shutil
 from multiprocessing import cpu_count
 
 import torch
-
-try:
-    import intel_extension_for_pytorch as ipex  # pylint: disable=import-error, unused-import
-
-    if torch.xpu.is_available():
-        from infer.modules.ipex import ipex_init
-
-        ipex_init()
-except Exception:  # pylint: disable=broad-exception-caught
-    pass
 import logging
 
 logger = logging.getLogger(__name__)
--- infer/modules/train/extract_f0.py
+++ infer/modules/train/extract_f0.py
@@ -29,27 +29,30 @@ def printt(strr):
     f.write("%s\n" % strr)
     f.flush()
 
+
 class FeatureInput(object):
     def __init__(self, samplerate=16000, hop_size=160):
         self.fs = samplerate
         self.hop = hop_size
+
         self.f0_bin = 256
         self.f0_max = 1100.0
         self.f0_min = 50.0
         self.f0_mel_min = 1127 * np.log(1 + self.f0_min / 700)
         self.f0_mel_max = 1127 * np.log(1 + self.f0_max / 700)
 
-    def compute_f0(self, path, f0_method, f0_min=1, f0_max=40000):
+    def compute_f0(self, path, f0_method):
         x = load_audio(path, self.fs)
-        if f0_method == "rmvpe+":
-            if not hasattr(self, "model_rmvpe"):
-                from infer.lib.rmvpe_plus import RMVPE_plus
-                print("Loading rmvpe+ model")
-                self.model_rmvpe = RMVPE_plus("assets/rmvpe/rmvpe.pt", is_half=is_half, device="cuda")
-            f0 = self.model_rmvpe.infer_from_audio_with_pitch(x, thred=0.03, f0_min=f0_min, f0_max=f0_max)
-        else:
-            raise ValueError("Unsupported f0 method: {}".format(f0_method))
-        
+        # p_len = x.shape[0] // self.hop
+        if f0_method == "rmvpe":
+            if hasattr(self, "model_rmvpe") == False:
+                from infer.lib.rmvpe import RMVPE
+
+                print("Loading rmvpe model")
+                self.model_rmvpe = RMVPE(
+                    "assets/rmvpe/rmvpe.pt", is_half=is_half, device="cuda"
+                )
+            f0 = self.model_rmvpe.infer_from_audio(x, thred=0.03)
         return f0
 
     def coarse_f0(self, f0):
@@ -58,6 +61,7 @@ class FeatureInput(object):
             self.f0_bin - 2
         ) / (self.f0_mel_max - self.f0_mel_min) + 1
 
+        # use 0 or 1
         f0_mel[f0_mel <= 1] = 1
         f0_mel[f0_mel > self.f0_bin - 1] = self.f0_bin - 1
         f0_coarse = np.rint(f0_mel).astype(int)
@@ -72,24 +76,36 @@ class FeatureInput(object):
             printt("no-f0-todo")
         else:
             printt("todo-f0-%s" % len(paths))
-            n = max(len(paths) // 5, 1)  # каждое n-е сообщение
+            n = max(len(paths) // 5, 1)  # 每个进程最多打印5条
             for idx, (inp_path, opt_path1, opt_path2) in enumerate(paths):
                 try:
                     if idx % n == 0:
-                        printt("f0ing, now-%s, all-%s, -%s" % (idx, len(paths), inp_path))
+                        printt("f0ing,now-%s,all-%s,-%s" % (idx, len(paths), inp_path))
                     if (
-                        os.path.exists(opt_path1 + ".npy") 
-                        and os.path.exists(opt_path2 + ".npy")
+                        os.path.exists(opt_path1 + ".npy") == True
+                        and os.path.exists(opt_path2 + ".npy") == True
                     ):
                         continue
                     featur_pit = self.compute_f0(inp_path, f0_method)
-                    np.save(opt_path2, featur_pit, allow_pickle=False)  # nsf
+                    np.save(
+                        opt_path2,
+                        featur_pit,
+                        allow_pickle=False,
+                    )  # nsf
                     coarse_pit = self.coarse_f0(featur_pit)
-                    np.save(opt_path1, coarse_pit, allow_pickle=False)  # ori
-                except Exception as e:
+                    np.save(
+                        opt_path1,
+                        coarse_pit,
+                        allow_pickle=False,
+                    )  # ori
+                except:
                     printt("f0fail-%s-%s-%s" % (idx, inp_path, traceback.format_exc()))
 
+
 if __name__ == "__main__":
+    # exp_dir=r"E:\codes\py39\dataset\mi-test"
+    # n_p=16
+    # f = open("%s/log_extract_f0.log"%exp_dir, "w")
     printt(" ".join(sys.argv))
     featureInput = FeatureInput()
     paths = []
@@ -107,6 +123,19 @@ if __name__ == "__main__":
         opt_path2 = "%s/%s" % (opt_root2, name)
         paths.append([inp_path, opt_path1, opt_path2])
     try:
-        featureInput.go(paths[i_part::n_part], "rmvpe+")
-    except Exception as e:
+        featureInput.go(paths[i_part::n_part], "rmvpe")
+    except:
         printt("f0_all_fail-%s" % (traceback.format_exc()))
+    # ps = []
+    # for i in range(n_p):
+    #     p = Process(
+    #         target=featureInput.go,
+    #         args=(
+    #             paths[i::n_p],
+    #             f0method,
+    #         ),
+    #     )
+    #     ps.append(p)
+    #     p.start()
+    # for i in range(n_p):
+    #     ps[i].join()
