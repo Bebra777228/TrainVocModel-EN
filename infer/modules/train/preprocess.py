a3bb7f4f1eee04f4d104a614447dcf5c1b1f476d
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-12-13 01:45:50 +0500

    1

--- infer/modules/train/preprocess.py
+++ infer/modules/train/preprocess.py
@@ -1,39 +1,43 @@
 import multiprocessing
 import os
 import sys
+import traceback
 
+import librosa
+import numpy as np
 from scipy import signal
+from scipy.io import wavfile
 
-now_dir = os.getcwd()
-sys.path.append(now_dir)
-print(*sys.argv[1:])
+sys.path.append(os.getcwd())
+
+from infer.lib.audio import load_audio
+from infer.lib.slicer2_gpu import Slicer
+
 inp_root = sys.argv[1]
 sr = int(sys.argv[2])
 n_p = int(sys.argv[3])
 exp_dir = sys.argv[4]
 noparallel = sys.argv[5] == "True"
 per = float(sys.argv[6])
-import os
-import traceback
+sr_trgt = sr
 
-import librosa
-import numpy as np
-from scipy.io import wavfile
-
-from infer.lib.audio import load_audio
-from infer.lib.slicer2 import Slicer
-
-f = open("%s/preprocess.log" % exp_dir, "a+")
+f = open(f"{exp_dir}/logfile.log", "a+")
 
 
-def println(strr):
+def printt(strr):
     print(strr)
-    f.write("%s\n" % strr)
+    f.write(f"{strr}\n")
     f.flush()
 
 
 class PreProcess:
-    def __init__(self, sr, exp_dir, per=3.7):
+    def __init__(self, sr, sr_trgt, exp_dir, per=3.0):
+        self.gt_wavs_dir = os.path.join(exp_dir, "data", "0_gt_wavs")
+        self.wavs16k_dir = os.path.join(exp_dir, "data", "1_16k_wavs")
+
+        os.makedirs(self.gt_wavs_dir, exist_ok=True)
+        os.makedirs(self.wavs16k_dir, exist_ok=True)
+
         self.slicer = Slicer(
             sr=sr,
             threshold=-42,
@@ -49,40 +53,41 @@ class PreProcess:
         self.tail = self.per + self.overlap
         self.max = 0.9
         self.alpha = 0.75
-        self.exp_dir = exp_dir
-        self.gt_wavs_dir = "%s/0_gt_wavs" % exp_dir
-        self.wavs16k_dir = "%s/1_16k_wavs" % exp_dir
-        os.makedirs(self.exp_dir, exist_ok=True)
-        os.makedirs(self.gt_wavs_dir, exist_ok=True)
-        os.makedirs(self.wavs16k_dir, exist_ok=True)
+        self.sr_trgt = sr_trgt
 
     def norm_write(self, tmp_audio, idx0, idx1):
         tmp_max = np.abs(tmp_audio).max()
         if tmp_max > 2.5:
-            print("%s-%s-%s-filtered" % (idx0, idx1, tmp_max))
+            printt(f"{idx0}-{idx1}-{tmp_max}-filtered")
             return
+
+        tmp_audio = librosa.resample(
+            tmp_audio, orig_sr=self.sr, target_sr=self.sr_trgt, res_type="soxr_vhq"
+        )
+
         tmp_audio = (tmp_audio / tmp_max * (self.max * self.alpha)) + (
             1 - self.alpha
         ) * tmp_audio
+
         wavfile.write(
-            "%s/%s_%s.wav" % (self.gt_wavs_dir, idx0, idx1),
-            self.sr,
+            f"{self.gt_wavs_dir}/{idx0}_{idx1}.wav",
+            self.sr_trgt,
             tmp_audio.astype(np.float32),
         )
+
         tmp_audio = librosa.resample(
-            tmp_audio, orig_sr=self.sr, target_sr=16000
-        )  # , res_type="soxr_vhq"
+            tmp_audio, orig_sr=self.sr, target_sr=16000, res_type="soxr_vhq"
+        )
+
         wavfile.write(
-            "%s/%s_%s.wav" % (self.wavs16k_dir, idx0, idx1),
+            f"{self.wavs16k_dir}/{idx0}_{idx1}.wav",
             16000,
             tmp_audio.astype(np.float32),
         )
 
     def pipeline(self, path, idx0):
         try:
-            audio = load_audio(path, self.sr)
-            # zero phased digital filter cause pre-ringing noise...
-            # audio = signal.filtfilt(self.bh, self.ah, audio)
+            audio = load_audio(path, self.sr_trgt)
             audio = signal.lfilter(self.bh, self.ah, audio)
 
             idx1 = 0
@@ -97,12 +102,12 @@ class PreProcess:
                         idx1 += 1
                     else:
                         tmp_audio = audio[start:]
+                        self.norm_write(tmp_audio, idx0, idx1)
                         idx1 += 1
                         break
-                self.norm_write(tmp_audio, idx0, idx1)
-            println("%s\t-> Success" % path)
-        except:
-            println("%s\t-> %s" % (path, traceback.format_exc()))
+            printt(f"{path}\t-> Success")
+        except Exception as e:
+            printt(f"{path}\t-> {traceback.format_exc()}")
 
     def pipeline_mp(self, infos):
         for path, idx0 in infos:
@@ -111,7 +116,7 @@ class PreProcess:
     def pipeline_mp_inp_dir(self, inp_root, n_p):
         try:
             infos = [
-                ("%s/%s" % (inp_root, name), idx)
+                (os.path.join(inp_root, name), idx)
                 for idx, name in enumerate(sorted(list(os.listdir(inp_root))))
             ]
             if noparallel:
@@ -125,18 +130,21 @@ class PreProcess:
                     )
                     ps.append(p)
                     p.start()
-                for i in range(n_p):
-                    ps[i].join()
-        except:
-            println("Fail. %s" % traceback.format_exc())
+                for p in ps:
+                    p.join()
+        except Exception as e:
+            printt(f"Ошибка! {traceback.format_exc()}")
+            sys.exit(1)
 
 
 def preprocess_trainset(inp_root, sr, n_p, exp_dir, per):
-    pp = PreProcess(sr, exp_dir, per)
-    println("start preprocess")
+    printt("Обработка датасета...")
+    pp = PreProcess(sr, sr_trgt, exp_dir, per)
+
     pp.pipeline_mp_inp_dir(inp_root, n_p)
-    println("end preprocess")
+    printt("Обработка успешно завершена!")
 
 
 if __name__ == "__main__":
     preprocess_trainset(inp_root, sr, n_p, exp_dir, per)
+    printt("\n\n")
