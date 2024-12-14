74498a238949528abdb8253623c8356d885d2cd7
Politrees <143968312+Bebra777228@users.noreply.github.com>
2024-12-15 02:14:24 +0500

    upd DataLoader

--- infer/modules/train/train.py
+++ infer/modules/train/train.py
@@ -167,7 +167,7 @@ def run(rank, n_gpus, hps, logger: logging.Logger):
         collate_fn = TextAudioCollate()
     train_loader = DataLoader(
         train_dataset,
-        num_workers=4,
+        num_workers=2,  # 4
         shuffle=False,
         pin_memory=True,
         collate_fn=collate_fn,
