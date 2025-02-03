c2f66572155d287e8d839532a221c07fd2f5cc0f
Player1444 <94734830+Player124413@users.noreply.github.com>
2025-02-03 18:25:12 +0300

    Update extract_feature-transformers.py

--- infer/modules/train/extract_feature-transformers.py
+++ infer/modules/train/extract_feature-transformers.py
@@ -6,7 +6,7 @@ import soundfile as sf
 import torch
 import torch.nn.functional as F
 import transformers
-from transformers import HubertModel, HubertConfig, Wav2Vec2FeatureExtractor
+from transformers import HubertModelWithFinalProj, HubertConfig, Wav2Vec2FeatureExtractor
 
 n_part = int(sys.argv[1])
 i_part = int(sys.argv[2])
@@ -70,7 +70,7 @@ if not os.path.exists(config_path) or not os.path.exists(model_file_path):
 # Load the HuBERT model and feature extractor from Transformers
 config = HubertConfig.from_pretrained(model_path)
 feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(model_path)
-model = HubertModel.from_pretrained(model_path, config=config)
+model = HubertModelWithFinalProj.from_pretrained(model_path, config=config)
 model = model.to(device)
 
 if is_half and device not in ["mps", "cpu"]:
