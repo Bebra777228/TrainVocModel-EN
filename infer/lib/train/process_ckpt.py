3be11a179fa137a186f37fef0ea0ca3edd816a3b
Player1444 <94734830+Player124413@users.noreply.github.com>
2025-02-06 22:08:14 +0300

    Update process_ckpt.py

--- infer/lib/train/process_ckpt.py
+++ infer/lib/train/process_ckpt.py
@@ -32,7 +32,7 @@ def savee(ckpt, sr, if_f0, name, epoch, version, hps):
             hps.model.upsample_kernel_sizes,
             hps.model.spk_embed_dim,
             hps.model.gin_channels,
-            hps.data.sampling_rate,
+            hps.data.sample_rate,
         ]
         opt["info"] = "%sepoch" % epoch
         opt["sr"] = sr
