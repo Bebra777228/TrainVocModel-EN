cf8d64d12a2114628f2a84dd0850ff6736b364b7
Player1444 <94734830+Player124413@users.noreply.github.com>
2025-02-06 17:12:42 +0300

    Update models_refinegan-for-launch-simplcup-pretrain.py

--- infer/lib/infer_pack/models_refinegan-for-launch-simplcup-pretrain.py
+++ infer/lib/infer_pack/models_refinegan-for-launch-simplcup-pretrain.py
@@ -862,17 +862,12 @@ class SynthesizerTrnMs768NSFsid(nn.Module):
             kernel_size,
             float(p_dropout),
         )
-        self.dec = GeneratorNSF(
-            inter_channels,
-            resblock,
-            resblock_kernel_sizes,
-            resblock_dilation_sizes,
-            upsample_rates,
-            upsample_initial_channel,
-            upsample_kernel_sizes,
-            gin_channels=gin_channels,
-            sr=sr,
-            is_half=kwargs["is_half"],
+        self.dec = RefineGANGenerator(
+            sample_rate=sr,
+            downsample_rates=upsample_rates[::-1],
+            upsample_rates=upsample_rates,
+            start_channels=16,
+            num_mels=inter_channels,
         )
         self.enc_q = PosteriorEncoder(
             spec_channels,
