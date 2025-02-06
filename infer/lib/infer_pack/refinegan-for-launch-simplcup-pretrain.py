a226ebed1ec0610499c5309bbfa4f442f9661908
Player1444 <94734830+Player124413@users.noreply.github.com>
2025-02-06 17:00:18 +0300

    Update refinegan-for-launch-simplcup-pretrain.py

--- infer/lib/infer_pack/refinegan-for-launch-simplcup-pretrain.py
+++ infer/lib/infer_pack/refinegan-for-launch-simplcup-pretrain.py
@@ -4,7 +4,7 @@ from torch.nn.utils.parametrizations import weight_norm
 from torch.nn.utils.parametrize import remove_parametrizations
 from torch.utils.checkpoint import checkpoint
 
-from rvc.lib.algorithm.commons import get_padding
+from infer.lib.infer_pack.commons import get_padding
 
 
 class ResBlock(torch.nn.Module):
